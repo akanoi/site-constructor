a029b37f826b792a224bd8cc35b79c183e61c4b1
Aleh Kanoika <aleh.i.kanoika@gmail.com>
2017-01-23 14:54:24 +0300

    Add validation in model.

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,8 @@ class Page < ActiveRecord::Base
   belongs_to :site
   has_many :widgets, dependent: :destroy
 
+  validates :title, :layout, :site_id, presence: true
+
   searchable do
     text :title
   end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,7 @@
 class Site < ActiveRecord::Base
   belongs_to :user
   has_many :pages, dependent: :destroy
+  validates :title, :description, :user_id, :menu, presence: true
 
   searchable do
     text :title, :boost => 2
